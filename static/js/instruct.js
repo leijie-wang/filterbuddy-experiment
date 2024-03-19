1417d22d305c886077c3dde63aa98a0349c7d087
Leijie Wang <leijiew@cs.washington.edu>
2024-03-19 09:17:51 -0700

    Finalized the template participants see in the tutorial.

--- static/js/instruct.js
+++ static/js/instruct.js
@@ -34,7 +34,7 @@ function addNewInstruction(){
         displayInstructions([{
             "name": "",
             "action": 1,
-            "rubric": "",
+            "rubric": "Remove texts that ...",
             "positives": [],
             "negatives": []
         }]);
